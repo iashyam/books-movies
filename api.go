b5e80193cfc4c5a61c1658e5b88bdf770991de82
iashyam <shyam10kwd@gmail.com>
2026-08-20 19:35:08 +0530

    adds CORS middleware

--- api.go
+++ api.go
@@ -9,6 +9,22 @@ import (
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+
+	// CORS middleware
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	})
+
 	DB, err := LoadDB(context.Background())
 	if err != nil {
 		panic(err)
